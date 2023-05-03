83311e402e9d6a56e39b6d93625bd18082cfad0f
Jasiukiewicz Tymon <JasiukiewiczTymon@outlook.fr>
2023-05-03 23:26:27 +0200

    Added live time update with websockers

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const http  = require('http');
 const url   = require('url');
 const fs    = require('fs');
 
+const { WebSocketServer, OPEN } = require('ws');
+
 const mime = require('mime');
 
 require('dotenv').config();
 
 const PORT      = process.env.PORT || 8080;
+const WSPORT    = process.env.WSPORT || 8090;
 const PROTOCOL  = process.env.PROTOCOL || 'http';
 const LOG       = process.env.LOG == 'true' || false;
+
+var WSSERVER = null;
 
 function serveFile(req, res) {
     const splitUrl = req.url.split('.');
@@ -19,7 +24,7 @@ function serveFile(req, res) {
     const q = url.parse(req.url, true);
     fs.readFile(`${__dirname}/public${q.path || '/'}`, function(err, data) {
         if (err) {
-            fs.readFile(`${__dirname}/error/404.html`, function(err, data) {
+            fs.readFile(`/error/404.html`, function(err, data) {
                 res.writeHead(404, { 'Content-Type': 'text/html' });
                 res.end(data);
             });
@@ -40,11 +45,42 @@ http.createServer(function (req, res) {
 
     serveFile(req, res);
 }).listen(PORT);
+console.log(`http listening on: https://localhost:${PORT}/`)
 
-https.createServer({
-    key: fs.readFileSync(`${__dirname}/cert/localhost.key`),
-    cert: fs.readFileSync(`${__dirname}/cert/localhost.crt`)
-}, function (req, res) {
-    serveFile(req, res);
-}).listen(PORT + 1);
+if (PROTOCOL == 'http') {
+    WSSERVER = new WebSocketServer({ port: WSPORT });
+    console.log(`websocket listening on: ws://localhost:${WSPORT}/`)
+}
+else {
+    https.createServer({
+        key: fs.readFileSync(`${__dirname}/cert/localhost.key`),
+        cert: fs.readFileSync(`${__dirname}/cert/localhost.crt`)
+    }, function (req, res) {
+        serveFile(req, res);
+    }).listen(PORT + 1);
+    console.log(`https listening on: https://localhost:${PORT + 1}/`)
 
+    const server = https.createServer({
+        cert: fs.readFileSync(`${__dirname}/cert/localhost.crt`),
+        key: fs.readFileSync(`${__dirname}/cert/localhost.key`)
+    });
+      
+    WSSERVER = new WebSocketServer({ server });
+      
+    server.listen(WSPORT + 1);
+    console.log(`websocket (secure) listening on: wss://localhost:${WSPORT + 1}/`)
+}
+
+fs.watch(`${__dirname}/public`, {
+    persistent: true,
+    recursive: true
+}, (event, filename) => {
+    if (filename) {
+        console.log(`Server socket update: ${filename}:${event}`)
+        WSSERVER.clients.forEach(client => {
+            if (client.readyState === OPEN) {
+                client.send(`${filename}:${event}`);
+            }
+        });
+    }
+});
